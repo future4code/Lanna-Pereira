59bd8604779eb71a981fcf723c0f6cca7179a6d1
Lanna-Brenda <lannabrnd@gmail.com>
2022-03-18 17:28:56 -0300

    atividades-node-concluidas

--- backend/node/exercicio1.js
+++
@@ -1,8 +0,0 @@
-const nome = process.argv[2] 
-const idade = Number(process.argv[3])
-
-
-
-console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
-
-console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade+7}`)
