9fdde97a14d08f997c799382282f7f5511d94acf
Lanna Brenda <89163834+Lanna-Brenda@users.noreply.github.com>
2022-08-10 17:40:22 -0300

    Update CartasFrente.js

--- Case-Tarot/src/components/CartasFrente.js
+++ Case-Tarot/src/components/CartasFrente.js
@@ -1,25 +1,28 @@
 import React, { useContext } from "react";
 import GlobalContext from "../global/globalContext";
 import styled from "styled-components";
-import img1 from "../img/img1.jpg"
 
 const ContainerPrincipal = styled.div`
-   display: flex;
-   flex-direction: row;
-   background-image: url(${img1});
+   width: 100%;
+   height: 100%;
 `
 const SubContainer = styled.div`
    display: grid;
    row-gap: 15px;
    column-gap: 15px;
-   grid-template-rows: repeat(4,1fr);
    grid-template-columns: repeat(8, 1fr);
 `
 
 const Imagem = styled.img`
     border-radius: 15px;
 `
-
+const Container4 = styled.div`
+ &:hover{
+    background-color: darkgrey;
+    border-radius: 15px;
+    align-items: center;
+ }
+`
 
 const CartasFrente = () => {
     const {states, imagemFrente} = useContext(GlobalContext)
@@ -28,9 +31,9 @@ console.log(tarot)
     const carta = tarot.cards?
     tarot.cards.map((card) => {
         return(
-            <div>
+            <Container4>
                 <Imagem src={imagemFrente + card.image} alt ={card.image}/>
-            </div>
+            </Container4>
         )
     })
 
